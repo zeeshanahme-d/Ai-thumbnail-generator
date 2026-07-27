cdffb4774904117e563a1f252bb8f0ee54811254
zeeshanahme-d <zeeshanahmed@gmail.com>
2026-07-27 17:33:01 +0500

    feat: refactor Cloudinary configuration for improved readability

--- server/src/config/cloudinary.ts
+++ server/src/config/cloudinary.ts
@@ -1,9 +1,11 @@
 import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
+const keys = () => ({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+cloudinary.config({ ...keys() });
 
 export default cloudinary;
