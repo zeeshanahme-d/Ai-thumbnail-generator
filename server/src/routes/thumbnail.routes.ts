75909051816c258f853b96c49214b7a68eddc950
zeeshanahme-d <dev.zeeshanahmed@gmail.com>
2026-07-28 18:54:58 +0500

    feat: refactor thumbnail routes to include authentication and improve structure

--- server/src/routes/thumbnail.routes.ts
+++ server/src/routes/thumbnail.routes.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import validate from "../middlewares/validate.js";
-import authenticationToken from "../middlewares/auth.middleware.js";
 import {
   generateGminiThumbnail,
   generateHFThumbnail,
+  getMyThumbnails,
+  softDeleteThumbnail,
 } from "../controllers/thumbnail.controller.js";
+import { generateThumbnailSchema } from "../validations/thumbnail.validation.js";
+import { uploadSingleImage } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
-router.post("/", authenticationToken, generateGminiThumbnail);
-router.post("/hf", authenticationToken, generateHFThumbnail);
+router.get("/", getMyThumbnails);
+router.post("/", uploadSingleImage("referenceImage"), validate(generateThumbnailSchema), generateGminiThumbnail);
+router.post("/hf", generateHFThumbnail);
+router.delete("/:id", softDeleteThumbnail);
 
 export default router;
