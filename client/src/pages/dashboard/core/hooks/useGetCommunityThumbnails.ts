992acc1c5239fd6135c7754756c89ee43210fcd5
zeeshanahme-d <dev.zeeshanahmed@gmail.com>
2026-07-30 17:29:04 +0500

    feat: add hooks for community, my, and recycle bin thumbnails, and implement like and publish functionality

---
+++ client/src/pages/dashboard/core/hooks/useGetCommunityThumbnails.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCommunityThumbnails } from "../_requests";
+import { thumbnailKeys } from "./query-keys";
+import type { PaginationParams } from "../_models";
+
+const useGetCommunityThumbnails = (params?: PaginationParams) => {
+  const { data, isPending, isError, error, refetch } = useQuery({
+    queryKey: thumbnailKeys.community(params),
+    queryFn: () => getCommunityThumbnails(params),
+    staleTime: 30 * 1000,
+  });
+
+  return {
+    data,
+    isPending,
+    isError,
+    error,
+    refetch,
+  };
+};
+
+export default useGetCommunityThumbnails;
