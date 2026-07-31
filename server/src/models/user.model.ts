3c29a4b07e949ff7b4fd60e8e88c08e4ca1ff09c
zeeshanahme-d <dev.zeeshanahmed@gmail.com>
2026-07-31 22:44:38 +0500

    feat: refactor authentication middleware for cleaner error handling and update user model with credits management

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -49,6 +49,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
       trim: true,
+      url: true
     },
 
     // Auth
@@ -76,7 +77,12 @@ const userSchema = new mongoose.Schema(
       default: UserPlan.Free,
     },
 
-    credits: {
+    totalcredits: {
+      type: Number,
+      default: 20,
+    },
+
+    creditsUsed: {
       type: Number,
       default: 0,
     },
@@ -103,7 +109,7 @@ const userSchema = new mongoose.Schema(
       default: 0,
     },
 
-    generationsResetAt: {
+    creditsResetAt: {
       type: Date,
       default: null,
     },
