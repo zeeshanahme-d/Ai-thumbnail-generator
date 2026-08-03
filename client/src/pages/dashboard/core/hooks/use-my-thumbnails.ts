d0a3cd6b29986613c6757599114cb61eafacea43
zeeshanahme-d <dev.zeeshanahmed@gmail.com>
2026-08-03 20:44:38 +0500

    feat: remove useMyThumbnails hook as part of code cleanup

--- client/src/pages/dashboard/core/hooks/use-my-thumbnails.ts
+++
@@ -1,9 +0,0 @@
-import useGetMyThumbnails from "./useGetMyThumbnails";
-
-export default function useMyThumbnails() {
-  const { data } = useGetMyThumbnails();
-  return {
-    data: data?.thumbnails || [],
-    isLoading: !data,
-  };
-}
