5e0cbe4169bce77c6231be14a0ed428ade86c445
zeeshanahme-d <dev.zeeshanahmed@gmail.com>
2026-08-04 21:46:44 +0500

    feat: refactor BackButton component to use Button for consistent styling and functionality

--- client/src/pages/auth/components/BackButton.tsx
+++ client/src/pages/auth/components/BackButton.tsx
@@ -1,17 +1,20 @@
 import { ArrowLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Button from "../../../components/Button";
 
 export default function BackButton() {
     const navigate = useNavigate();
 
     return (
-        <button
+        <Button
             type="button"
+            variant="secondary"
+            fullWidth={false}
             onClick={() => navigate(-1)}
-            className="flex items-center gap-1.5 text-sm font-medium text-text-secondary transition hover:text-text-primary"
+            className="border-none p-0! hover:bg-transparent!"
         >
             <ArrowLeft size={16} />
             Back
-        </button>
+        </Button>
     );
 }
