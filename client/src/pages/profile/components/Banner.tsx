ecbdb80a747dcb361cb7dc63b2c164f883ab933c
zeeshanahme-d <dev.zeeshanahmed@gmail.com>
2026-08-08 23:00:02 +0500

    feat: update Banner component styles for improved layout and responsiveness

--- client/src/pages/profile/components/Banner.tsx
+++ client/src/pages/profile/components/Banner.tsx
@@ -3,9 +3,9 @@ interface BannerProps {
 }
 
 const Banner = ({ className }: BannerProps) => {
-    return <div className={`h-[140px] sm:h-[200px] relative overflow-hidden bg-linear-to-b from-primary via-[#c1121f] to-[#9c0b01] ${className}`}>
-        <div aria-hidden="true" className="absolute top-[-40px] right-[10%] w-[200px] h-[200px] rounded-full bg-white/10"></div>
-        <div aria-hidden="true" className="absolute bottom-[-30px] left-[20%] w-[150px] h-[150px] rounded-full bg-white/5"></div>
+    return <div className={`h-35 sm:h-50 relative overflow-hidden bg-linear-to-b from-primary via-[#c1121f] to-[#9c0b01] ${className}`}>
+        <div aria-hidden="true" className="absolute -top-10 right-[10%] w-50 h-50 rounded-full bg-white/10"></div>
+        <div aria-hidden="true" className="absolute -bottom-7.5 left-[20%] w-37.5 h-37.5 rounded-full bg-white/5"></div>
     </div>
 }
 
