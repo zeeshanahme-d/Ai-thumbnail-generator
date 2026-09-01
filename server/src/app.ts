f762e03b427314b9e69218ca1697360f9f527543
zeeshanahme-d <dev.zeeshanahmed@gmail.com>
2026-09-01 20:14:04 +0500

    feat: integrate helmet middleware for enhanced security and set JSON payload limit

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import cors from "cors";
+import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import authenticationToken from "./middlewares/auth.middleware.js";
 import authRouter from "./routes/auth.routes.js";
@@ -10,7 +11,11 @@ import thumbnailPublicRouter from "./routes/thumbnail-public.routes.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(helmet());
+
+app.use(express.json({
+  limit: "2mb",
+}));
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
